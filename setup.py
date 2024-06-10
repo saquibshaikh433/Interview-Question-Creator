6e9ff6e4e31afa4edb4612dbae97d57376888558
saquibshaikh433 <saquibshaikh433@gmail.com>
2024-06-10 15:55:34 +0530

    adding templates

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name = "Generative AI Project Question Creator",
+    version = "0.0.0",
+    description = "this project interview question creator take the pdf and create question for interview",
+    author = " Najmus Saquib",
+    author_email = "greatlearning433@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
